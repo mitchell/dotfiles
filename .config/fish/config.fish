b86ffd6c13b0ff8054f6c10a0f9b0fe340d4c979
Mitchell Simon <mitchelljfsimon@gmail.com>
2019-08-23 09:43:41 -0400

    Refactor PATH var definition

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
 set -gx GOPATH $HOME/Documents/go
 set -gx GOBIN $GOPATH/bin
-set -gx PATH $PATH $GOBIN $HOME/.pub-cache/bin $HOME/Documents/bash_scripts $HOME/Library/Python/2.7/bin $HOME/.n/bin $HOME/Documents/flutter/bin $HOME/.cargo/bin
+set -gx PATH $PATH $GOBIN \
+    $HOME/.pub-cache/bin \
+    $HOME/Documents/bash_scripts \
+    $HOME/Library/Python/2.7/bin \
+    $HOME/.n/bin \
+    $HOME/Documents/flutter/bin \
+    $HOME/.cargo/bin
 
 set -g fish_escape_delay_ms 10
 
