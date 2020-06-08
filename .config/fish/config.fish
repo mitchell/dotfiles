8c77a00b0182491efb742572235ccad558dca984
mitchell <mitchelljfsimon@gmail.com>
2020-06-08 14:30:33 -0400

    Refactor fish config and universal vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,19 @@
-set -gx EDITOR nvim
-
-set -gx GOPATH $HOME/code/go
-set -gx GOBIN $GOPATH/bin
+# variables
+set -gx AWS_SDK_LOAD_CONFIG true
 
 set -gx DOTNET_ENVIRONMENT Development
 set -gx DOTNET_CLI_TELEMETRY_OPTOUT true
 set -gx DOTNET_ROOT $HOME/.asdf/installs/dotnet-core/(command -sq dotnet; and dotnet --version)
+
+set -gx EDITOR nvim
+
+set -gx FZF_CTRL_T_COMMAND 'ag --hidden --ignore .git -g ""'
+set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+
+set -gx GOPATH $HOME/code/go
+set -gx GOBIN $GOPATH/bin
+
+set -gx LIBVIRT_DEFAULT_URI 'qemu:///system'
 
 set -gx PATH $PATH $GOBIN \
     /snap/bin \
@@ -16,64 +24,53 @@ set -gx PATH $PATH $GOBIN \
     $HOME/.cargo/bin \
     $HOME/.dotnet/tools
 
-set -gx UNAME (uname)
+set -g uname (uname)
 
+# source imports 
 command -sq kitty; and kitty + complete setup fish | source
 
-switch "$UNAME"
-    case 'Linux'
+switch $uname
+    case Linux
         test -e ~/.asdf/asdf.fish; and source ~/.asdf/asdf.fish
-    case 'Darwin'
-        test -e (brew --prefix asdf)/asdf.fish; and source (brew --prefix asdf)/asdf.fish
+    case Darwin
+        command -sq brew
+        and test -e (brew --prefix asdf)/asdf.fish
+        and source (brew --prefix asdf)/asdf.fish
 end
 
 # The next line updates PATH for the Google Cloud SDK.
 # if test -f '/Users/m/Documents/google-cloud-sdk/path.fish.inc'; source '/Users/m/Documents/google-cloud-sdk/path.fish.inc'; end
 
 # aliases
-function cp; rsync -aP $argv; end
+alias cp 'rsync -aP'
+alias dm 'docker-machine'
+alias v  'nvim (fzf)'
+alias vg 'vagrant'
 
-function dm; docker-machine $argv; end
+switch $uname
+    case Linux
+        alias cat      'batcat --theme ansi-dark'
+        alias goland   'command goland'
+        alias rider    'command rider'
+        alias webstorm 'command webstorm'
+    case Darwin
+        alias cat      'bat --theme ansi-dark'
+        alias goland   'open -a Goland'
+        alias rider    'open -a Rider'
+        alias webstorm 'open -a Webstorm'
+end
 
 function dm-env; eval (docker-machine env $argv); end
 
-function v; nvim (fzf); end
+function temp -a ft
+  nvim temp.$ft
 
-function vg; vagrant $argv; end
+  switch $uname
+      case Linux
+          xclip -selection clipboard < temp.$ft
+      case Darwin
+          pbcopy < temp.$ft
+  end
 
-function cat
-    switch "$UNAME"
-        case 'Linux'
-            batcat --theme ansi-dark $argv
-        case 'Darwin'
-            bat --theme ansi-dark $argv
-    end
+  rm temp.$ft
 end
-
-function goland
-    switch "$UNAME"
-        case 'Linux'
-            command goland $argv
-        case 'Darwin'
-            open -a Goland $argv
-    end
-end
-
-function rider
-    switch "$UNAME"
-        case 'Linux'
-            command rider $argv
-        case 'Darwin'
-            open -a Rider $argv
-    end
-end
-
-function webstorm
-    switch "$UNAME"
-        case 'Linux'
-            command webstorm $argv
-        case 'Darwin'
-            open -a Webstorm $argv
-    end
-end
-
