2f5438dc21fab36ba973a9eafbeee3f0d3b81ce6
mitchell <mitchelljfsimon@gmail.com>
2020-08-15 19:06:43 -0400

    Refactor push_tasks and add send_push,timetask functions

---
+++ .config/fish/functions/send_push.fish
@@ -0,0 +1,11 @@
+function send_push -a message -d 'Send a push notification to your phone via pushover (optional delay as second arg)'
+  if test -n "$argv[2]"; sleep $argv[2]; end
+
+  set payload "{\"token\":\"$pushover_token\",\"user\":\"$pushover_user\",\"title\":\"Tasks\",\"message\":$message}"
+
+  curl \
+    -X POST \
+    -H 'Content-Type: application/json' \
+    -d $payload \
+    https://api.pushover.net/1/messages.json
+end
