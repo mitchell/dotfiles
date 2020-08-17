116620843a01eb221491c41b0041b3a6070b7df4
mitchell <mitchelljfsimon@gmail.com>
2020-08-17 17:24:14 -0400

    Add more attribtutes to push_tasks.fish payload

--- .config/fish/functions/push_tasks.fish
+++ .config/fish/functions/push_tasks.fish
@@ -2,7 +2,7 @@ function push_tasks -d 'Push tasks as a notification to your phone'
     set tasks (task +READY export |
                    jq 'sort_by(.urgency) |
                        reverse |
-                       map(@text "\(.description)\nLink: \(.link)") |
+                       map(@text "\(.description)\nLink: \(.link)\nWiki: http://wiki.m/\(.wiki)\nDue: \(.due)") |
                        join("\n\n")')
     send_push $tasks
 end
