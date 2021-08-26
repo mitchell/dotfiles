183e5801b6592974631696172df9cc109eb4f827
mitchell <m@mjfs.us>
2021-08-26 13:27:32 -0400

    Change default browser

--- .config/fish/functions/define_global_variables.fish
+++ .config/fish/functions/define_global_variables.fish
@@ -5,7 +5,7 @@ function define_global_variables -d 'Defines all and exclusively globally export
     set -gx DOTNET_CLI_TELEMETRY_OPTOUT true
 
     set -gx EDITOR nvim
-    set -gx BROWSER qutebrowser
+    set -gx BROWSER firefox
 
     set -gx FZF_CTRL_T_COMMAND 'ag --hidden --ignore .git -g ""'
     set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
