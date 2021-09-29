fbe9eb511e177f95f066903b58eb952cd9f018e9
mitchell <m@mjfs.us>
2021-09-29 11:50:51 -0400

    Modify how btre works and add sysz to default arch provision list

--- .config/fish/functions/btre.fish
+++ .config/fish/functions/btre.fish
@@ -1,20 +1,21 @@
 function btre -a device -d 'Reconnect to a known bluetooth device'
-    set -l devices 00:1B:66:E3:07:3A CC:98:8B:3E:6B:9D
+    set -l devices CC:98:8B:3E:6B:9D 00:1B:66:E3:07:3A
+    set -l index 1
+
+    if test -n "$device"
+        set index $device
+    end
 
     bluetoothctl power on
 
-    for device in $devices
-        bluetoothctl remove $device
-    end
+    bluetoothctl remove $devices[$index]
 
     nohup bluetoothctl scan on >/dev/null &
+    sleep 2
 
-    for device in $devices
-        bluetoothctl pair $device
-        sleep 1
-        bluetoothctl trust $device
-        bluetoothctl connect $device
-    end
+    bluetoothctl pair $devices[$index]
+    bluetoothctl trust $devices[$index]
+    bluetoothctl connect $devices[$index]
 
     pkill bluetoothctl
 end
