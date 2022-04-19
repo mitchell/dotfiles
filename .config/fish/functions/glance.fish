ab9f80ce40020aad7fa6d69cdfdf9321e36e17b7
mitchell <m@mjfs.us>
2022-04-19 01:51:49 -0400

    Unsticky mpv by default; add venator to glances

--- .config/fish/functions/glance.fish
+++ .config/fish/functions/glance.fish
@@ -2,6 +2,7 @@ function glance -d 'Open glances on a preset of machines'
     tmux new -s glance mosh arch@khetanna.lan -- glances --disable-process --sparkline\; \
         neww mosh arch@kamino.lan -- glances --disable-process --sparkline\; \
         neww mosh arch@marauder.lan -- glances --disable-process --sparkline\; \
-        neww mosh serenity.lan -- glances --disable-process --sparkline
+        neww mosh serenity.lan -- glances --sparkline\; \
+        neww mosh venator.lan -- glances --sparkline
     or tmux attach-session -t glance
 end
