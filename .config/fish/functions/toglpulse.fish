81e8b0c49ccbd2393cf099d36543a7616bfe82b7
mitchell <m@mjfs.us>
2022-09-23 01:33:26 -0400

    Set default MPV dimensions and improve toglpulse

--- .config/fish/functions/toglpulse.fish
+++ .config/fish/functions/toglpulse.fish
@@ -1,10 +1,13 @@
 function toglpulse
+    killall slack Discord DiscordCanary
+
     if test -e ~/.config/pulse/client.conf
         rm -rf ~/.config/pulse/client.conf
-        ssh venator.lan systemctl --user restart pipewire-pulse; or ssh venator.lan nssm restart pulseaudio
+        ssh -o StrictHostKeyChecking=no venator.lan systemctl --user restart pipewire-pulse
+        or ssh -o StrictHostKeyChecking=no venator.lan nssm restart pulseaudio
         rebar
     else
-        pkill -fi easyeffects; or pkill -fi -9 easyeffects
+        killall easyeffects pavucontrol; or killall -9 easyeffects pavucontrol
         echo "default-server = venator.lan" > ~/.config/pulse/client.conf
         systemctl --user stop pipewire pipewire-pulse wireplumber
         rebar
