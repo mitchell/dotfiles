19cb58d9617e77d9270d528f7387dcee44502bcd
mitchell <m@mjfs.us>
2023-02-17 16:58:18 -0500

    Add fish and kitty keybinds

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,8 @@ function fish_user_key_bindings
     bind --mode insert ,j 'joplin; commandline -f repaint'
     bind --mode insert ,p 'nvim +"Telescope git_files"; commandline -f repaint'
     bind --mode insert ,f 'nvim +"Telescope find_files"; commandline -f repaint'
+    bind --mode insert ,m 'nvim +Neotree; commandline -f repaint'
+    bind --mode insert ,v 'neovide; commandline -f repaint'
     bind --mode insert ,n 'n; commandline -f repaint'
     bind --mode insert ,s 'sysz; commandline -f repaint'
 end
