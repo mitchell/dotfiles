79deb0496ee27243afe8b565b5da581cc584a849
mitchell <m@mjfs.us>
2024-07-20 00:48:33 -0400

    m-cloud changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,12 @@ function configure_fish
     # Determine OS
     set uname (uname)
 
-    # Set umask
-    umask 077
-
     # Begin profile init
     # (These functions are defined in the functions/ directory.)
     define_global_variables
     import_sources $uname
     define_aliases $uname
-    ssh_agent_startup
-    starship init fish | source
     zoxide init fish | source
-    fzf --fish | source
 end
 
 configure_fish
