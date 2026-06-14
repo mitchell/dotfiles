e33c60c7aa0f9ab1a3d59361c04838716a8fbf51
mitchell <m@mjfs.us>
2026-06-14 18:22:58 -0400

    chore(fish): replace asdf with mise

--- .config/fish/conf.d/import_sources.fish
+++ .config/fish/conf.d/import_sources.fish
@@ -1,16 +1,5 @@
 ssh_agent_startup
 
-# ASDF config start
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-fish_add_path $_asdf_shims
-set --erase _asdf_shims
-# ASDF config end
-
 test -e /opt/homebrew/bin/brew
 and /opt/homebrew/bin/brew shellenv | source -
 
@@ -20,3 +9,4 @@ if status is-interactive
 end
 
 command -q pyenv; and pyenv init - | source
+command -q mise; and mise activate fish | source
